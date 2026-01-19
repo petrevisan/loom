a277164d7ccc2e0b49599ad1115910470e4a031b
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2026-01-18 21:55:57 -0400

    init

--- forge.config.ts
+++ forge.config.ts
@@ -20,25 +20,22 @@ const config: ForgeConfig = {
   ],
   plugins: [
     new VitePlugin({
-      // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-      // If you are familiar with Vite configuration, it will look really familiar.
       build: [
         {
-          // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
           entry: 'src/main.ts',
-          config: 'vite.main.config.ts',
+          config: 'vite.main.config.mts',
           target: 'main',
         },
         {
           entry: 'src/preload.ts',
-          config: 'vite.preload.config.ts',
+          config: 'vite.preload.config.mts',
           target: 'preload',
         },
       ],
       renderer: [
         {
           name: 'main_window',
-          config: 'vite.renderer.config.ts',
+          config: 'vite.renderer.config.mts',
         },
       ],
     }),
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -27,7 +27,13 @@
  */
 
 import './index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { HelloWorld } from './components/HelloWorld';
 
-console.log(
-  '👋 This message is being logged by "renderer.ts", included via Vite',
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <HelloWorld />
+  </React.StrictMode>
 );
